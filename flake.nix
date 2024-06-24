11a1155ee1035c2212c1d500b766fa738ca8d63c
Leah <45321184+ChaoticLeah@users.noreply.github.com>
2024-06-24 22:39:51 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    # NixOS official package source, here using the nixos-23.11 branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    unstablenixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  };
+
+  outputs = { self, nixpkgs, unstablenixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
